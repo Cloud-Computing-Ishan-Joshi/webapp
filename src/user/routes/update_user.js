274ee7c3aabf19ad080f73895b585114b2ea061b
Ishan Joshi <ishan25j@gmail.com>
2024-02-08 10:31:25 -0500

    Validation for the Update Request

--- src/user/routes/update_user.js
+++ src/user/routes/update_user.js
@@ -1,6 +1,6 @@
 // update user
 const express = require('express');
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const db = require('../../database/db');
 const validate_body = require('../middlewares/validate_body');
@@ -10,18 +10,18 @@ const auth = require('../middlewares/auth');
 const router = express.Router();
 
 const validate = [
-    body('first_name').isString(),
-    body('last_name').isString(),
-    body('password').isLength({ min: 6 }),
-    body('username').isEmail()
+    check('first_name').optional({ checkFalsy: true }).isString(),
+    check('last_name').optional({ checkFalsy: true }).isString(),
+    check('password').optional({ checkFalsy: true }).isLength({ min: 6 }),
+    check('username').optional({ checkFalsy: true }).isEmail()
 ];
 
 router.put('/self', validate_method, validate_body, auth, validate, async(req, res) => {
     res.set('cache-control', 'no-cache');
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //     return res.status(400).send();
-    // }
+    const errors = validationResult(req);
+    if (!errors.isEmpty() || req.body.username) {
+        return res.status(400).send();
+    }
     try {
         const user = await db.models.User.findOne({
             where: {

d87f12d10e7db2b96f1105a9c1a4e4bc47fbc862
Ishan Joshi <ishan25j@gmail.com>
2024-02-15 12:24:03 -0500

    User.sync remove alter and edited User Test

--- src/__test__/all.test.js
+++ src/__test__/all.test.js
@@ -48,14 +48,14 @@ describe('Healthz check endpoint', () => {
     });
 });
 
-const first_name = faker.name.firstName();
-const last_name = faker.name.lastName();
-const username = faker.internet.email();
-const password = "test@12345";
-const first_name_update = faker.name.firstName();
-const last_name_update = faker.name.lastName();
 
 describe('User endpoint', () => {
+    const first_name = faker.name.firstName();
+    const last_name = faker.name.lastName();
+    const username = faker.internet.email();
+    const password = "test@12345";
+    const first_name_update = faker.name.firstName();
+    const last_name_update = faker.name.lastName();
     test('should return 201 Status when user is created', async() => {
         const request_body = {
             first_name: first_name,
@@ -114,8 +114,8 @@ describe('User endpoint', () => {
         try {
             const response = await request(app).get('/v1/user/self').auth(basic_auth.username, basic_auth.password);
             expect(response.statusCode).toBe(200);
-            expect(response.body.first_name).not.toBe(first_name_update);
-            expect(response.body.last_name).not.toBe(last_name_update);
+            expect(response.body.first_name).toBe(first_name_update);
+            expect(response.body.last_name).toBe(last_name_update);
             expect(response.headers['cache-control']).toBe('no-cache');
         } catch (err) {
             console.log(err);

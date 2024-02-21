bb97358d54e092505d0dace01847b03fb8c0f7bf
Ishan25j <ishan25j@gmail.com>
2024-02-21 17:56:55 -0500

    workflow -> image creation worflow

--- machine_image/packer/gcp.pkr.hcl
+++ machine_image/packer/gcp.pkr.hcl
@@ -25,12 +25,12 @@ variable "image_family" {
 
 variable "image_name" {
   type    = string
-  default = "centos-stream-8"
+  default = "centos-stream-8-custom-image"
 }
 
 variable "ssh_username" {
   type    = string
-  default = "root"
+  default = "centos"
 }
 
 variable "image_storage_locations" {
@@ -43,30 +43,44 @@ variable "network" {
   default = "default"
 }
 
+variable "account_file" {
+  type      = string
+  default   = "/home/ishanjoshi7i/Downloads/dev-1-415017-ab007b3e3a97.json"
+}
+
 source "googlecompute" "centos" {
   project_id   = var.project_id
   zone         = var.zone
-  source_image_family = var.image_family
-  # account_file = var.account_file
+#   source_image_family = var.image_family
+  account_file = var.account_file
   ssh_username            = var.ssh_username
   image_name              = var.image_name
   network                = "default"
   image_storage_locations = var.image_storage_locations
+  source_image = "centos-stream-8-v20240110"
+  disk_size = 20
+  machine_type = "n1-standard-1"
+
 }
 
 build {
   sources = ["source.googlecompute.centos"]
   provisioner "shell" {
+    # script = "/home/ishanjoshi7i/cloud-computing/webapp/machine_image/packer/scripts/install.sh"
     script = "/home/runner/work/webapp/webapp/machine_image/packer/scripts/install.sh"
   }
+
   # copy the file to the instance before running the script through github actions
   provisioner "file" {
     source      = "/home/runner/work/webapp/webapp/webapp.zip"
-    destination = "/tmp/packer/webapp.zip"
+    # source      = "/home/ishanjoshi7i/cloud/webapp.zip"
+    # destination = "/tmp/packer/webapp.zip"
+    destination = "/tmp/webapp.zip"
   }
 
   # After copy the file, run the script
   provisioner "shell" {
     script = "/home/runner/work/webapp/webapp/machine_image/packer/scripts/postprocess.sh"
+    # script = "/home/ishanjoshi7i/cloud-computing/webapp/machine_image/packer/scripts/postprocess.sh"
   }
 }
--- machine_image/packer/scripts/postprocess.sh
+++ machine_image/packer/scripts/postprocess.sh
@@ -5,29 +5,48 @@ sudo groupadd -r csye6225
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
 
 # Unzip the file /tmp/packer/webapp.zip to /var/webapp
+# See the webapp.zip folder in /tmp/packer/webapp.zip using echo
 sudo mkdir -p /var/webapp
-sudo unzip /tmp/packer/webapp.zip -d /var/webapp
+# echo $(ls -la /tmp/)
+sudo unzip /tmp/webapp.zip -d /var/webapp
+sudo chmod 755 /var/webapp
+# echo $(ls -la /var/webapp/webapp)
 
 # Change the ownership of /var/webapp to csye6225:csye6225
 sudo chown -R csye6225:csye6225 /var/webapp
 
 # Create env file to /var/webapp/.env
-sudo touch /var/webapp/.env
-sudo echo "PORT=5432" > /var/webapp/.env
-sudo echo "DB_USERNAME=centos_user" >> /var/webapp/.env
-sudo echo "DB_PASSWORD=centos_password" >> /var/webapp/.env
-sudo echo "DB_NAME=centos_db" >> /var/webapp/.env
-sudo echo "DB_HOST=localhost" >> /var/webapp/.env
-sudo echo "DB_PORT=5432" >> /var/webapp/.env
+# sudo touch /var/webapp/webapp/.env
+# sudo chmod 755 /var/webapp/webapp/.env
+# sudo echo "PORT=5432" > /var/webapp/webapp/.env
+# sudo echo "DB_USERNAME=centos_user" >> /var/webapp/webapp/.env
+# sudo echo "DB_PASSWORD=centos_password" >> /var/webapp/webapp/.env
+# sudo echo "DB_NAME=centos_db" >> /var/webapp/webapp/.env
+# sudo echo "DB_HOST=localhost" >> /var/webapp/webapp/.env
+# sudo echo "DB_PORT=5432" >> /var/webapp/webapp/.env
+
+# Export environment variables
+
+
+
+# sudo echo "PORT=5432" | sudo tee -a /var/webapp/webapp/.env
+# sudo echo "DB_USER=centos_user" | sudo tee -a /var/webapp/webapp/.env
+# sudo echo "DB_PASSWORD=centos_password" | sudo tee -a /var/webapp/webapp/.env
+# sudo echo "DB_NAME=centos_db" | sudo tee -a /var/webapp/webapp/.env
+# sudo echo "DB_HOST=localhost" | sudo tee -a /var/webapp/webapp/.env
+# sudo echo "DB_PORT=5432" | sudo tee -a /var/webapp/webapp/.env
+
+# sudo chown csye6225:csye6225 /var/webapp/webapp/.env
 
 # Install the dependencies for the webapp npm
-sudo npm install --prefix /var/webapp
+sudo npm install --prefix /var/webapp/webapp
 
 
 # Use a systemd service file present in /var/webapp/machine_image/service/webapp.service to start the webapp service
-sudo cp /var/webapp/machine_image/service/webapp.service /etc/systemd/system/webapp.service
+sudo cp /var/webapp/webapp/machine_image/service/webapp.service /etc/systemd/system/webapp.service
 sudo systemctl daemon-reload
 sudo systemctl start webapp
 sudo systemctl enable webapp
 sudo systemctl status webapp
+sudo journalctl -xe
 

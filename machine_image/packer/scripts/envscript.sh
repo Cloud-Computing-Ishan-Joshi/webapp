5aed4356fd23c1409462ec79b85fd898716fda5c
ishan25j <ishan25j@gmail.com>
2024-02-26 13:19:56 -0500

    souce env on srv start

---
+++ machine_image/packer/scripts/envscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Source the .env file
+source /var/webapp/.env
+
+# Export environment variables
+export DB_HOST=$DB_HOST
+export DB_USER=$DB_USER
+export DB_PASSWORD=$DB_PASSWORD
+export DB_NAME=$DB_NAME
+export DB_PORT=$DB_PORT
+export PORT=$PORT
+export NODE_ENV=$NODE_ENV
